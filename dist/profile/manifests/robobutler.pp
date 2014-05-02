1eae0915e1c513379ec8ff60df73efcf6e144943
Kohsuke Kawaguchi <kk@kohsuke.org>
2014-05-01 17:02:31 -0700

    Persist the HTML reports directory of robobutler
    Directory gets created on the host and then bind-mounted in

--- dist/profile/manifests/robobutler.pp
+++ dist/profile/manifests/robobutler.pp
@@ -5,9 +5,16 @@
 class profile::robobutler (
   # all injected from hiera
   $nick,
-  $password
+  $password,
+  $logdir = '/var/lib/butlerbot'
 ) {
   include 'docker'
+
+  file { $logdir:
+    ensure => directory,
+    owner  => 500,
+    mode   => '0755'
+  }
 
   docker::image { 'jenkinsciinfra/butlerbot':
     image_tag => 'build6',
@@ -16,6 +23,7 @@ class profile::robobutler (
   docker::run { 'butlerbot':
     command => undef,
     image   => 'jenkinsciinfra/butlerbot',
-    env     => ["NICK=${nick}","PASSWORD=${password}"],
+    env     => ["NICK=${nick}","PASSWORD=${password}","HTML_DIR==${logdir}"],
+    volumes => [$logdir]
   }
 }
--- spec/classes/profile/robobutler_spec.rb
+++ spec/classes/profile/robobutler_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe 'profile::robobutler' do
   it { should contain_class 'docker' }
+  it { should contain_file '/var/lib/butlerbot'}
 end
