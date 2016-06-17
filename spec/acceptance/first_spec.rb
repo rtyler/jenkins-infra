1feb5cb38efef022af4449e83e258cb93ea0a1a0
R. Tyler Croy <tyler@monkeypox.org>
2016-06-17 08:04:30 -0700

    Start experimenting with running beaker-rspec
        bundle exec rake beaker

---
+++ spec/acceptance/first_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper_acceptance'
+
+describe 'a simple test' do
+  let(:manifest) do
+    'include role::spinach'
+  end
+
+  describe package('mysql-server') do
+    it { is_expected.to be_installed }
+  end
+
+  it 'should apply the manifest cleanly' do
+    pending
+    apply_manifest(manifest, :expect_changes=> true)
+
+    # run a second time without changes
+    apply_manifest(manifest, :catch_changes => true)
+  end
+end
---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,36 @@
+require 'beaker-rspec'
+
+# Install Puppet on all hosts
+#hosts.each do |host|
+#  install_puppet_on host, :default_action => 'gem_install'
+#end
+#
+PE_VERSION = '2016.1'
+
+RSpec.configure do |c|
+  module_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+
+  c.formatter = :documentation
+
+  c.before :suite do
+    fetch_pe(hosts)
+    install_pe(hosts)
+    # Install module to all hosts
+    hosts.each do |host|
+      next
+
+      dist_dir = File.join(module_root, 'dist')
+      Dir.foreach(dist_dir) do |d|
+        module_dir = File.join(dist_dir, d)
+        next unless File.directory? module_dir
+        next if d.start_with? '.'
+
+        install_dev_puppet_module_on(host,
+          :source => module_dir,
+          :module_name => d,
+          :target_module_path => "/etc/puppet/modules/")
+      end
+    end
+  end
+end
+
